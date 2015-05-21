6aaa905adf3cec0bf8954bd0e9bc18fc1e9d8d37
Avihay123 <avihay_cohen@campus.technion.ac.il>
2015-05-21 14:24:01 +0300

    commit

--- app/src/main/java/com/mycompany/veez/CreateListActivity.java
+++ app/src/main/java/com/mycompany/veez/CreateListActivity.java
@@ -3,12 +3,11 @@ package com.mycompany.veez;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class CreateListActivity extends Activity{
+public class CreateListActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_list);
     }
-
 }
