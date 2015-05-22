ce708c143e9a059839249ce6c81bea0d20728586
Avihay123 <avihay_cohen@campus.technion.ac.il>
2015-05-22 18:11:35 +0300

    commit

---
+++ app/src/main/java/com/mycompany/veez/AddItemActivity.java
@@ -0,0 +1,13 @@
+package com.mycompany.veez;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddItemActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+}
