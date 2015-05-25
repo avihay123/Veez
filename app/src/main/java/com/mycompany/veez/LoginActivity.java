12f46d9771320ed1ee14b8e1d98c1e3d150a71d5
Avihay123 <avihay_cohen@campus.technion.ac.il>
2015-05-25 13:45:11 +0300

    commit -  facebook_login

--- app/src/main/java/com/mycompany/veez/LoginActivity.java
+++ app/src/main/java/com/mycompany/veez/LoginActivity.java
@@ -1,30 +1,92 @@
 package com.mycompany.veez;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+
+import com.facebook.ProfileTracker;
+import com.facebook.appevents.AppEventsLogger;
+
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 
 public class LoginActivity extends Activity {
 
-    private Button b_login;
+    //    private Button b_login;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private ProfileTracker profileTracker;
+    private AccessTokenTracker accessTokenTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        //TODO for debug--------------------------------------------------------------------
+
+        //Intent intent = new Intent(getApplicationContext(), MyListsActivity.class);
+        //startActivity(intent);
+        //finish();
+        //------------------------------------------------------------------------------------
+
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                updateWithToken(currentAccessToken);
+            }
+        };
+
+
         setContentView(R.layout.activity_login);
 
-        b_login = (Button) findViewById(R.id.b_login);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        callbackManager = CallbackManager.Factory.create();
 
-        b_login.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                setContentView(R.layout.activity_my_lists);
+        // Other app specific specialization
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Intent intent = new Intent(getApplicationContext(), MyListsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), "login failed", Toast.LENGTH_SHORT).show();
+                //TODO
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+                //TODO App code
             }
         });
+    }
+
+    private void updateWithToken(AccessToken currentAccessToken) {
+        if (currentAccessToken != null) {
+            Intent intent = new Intent(getApplicationContext(), MyListsActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 
     @Override
@@ -48,4 +110,27 @@ public class LoginActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+
 }
