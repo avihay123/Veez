7ff92a9e29fbc8564b1d05511726b0a939ec4b7a
Avihay123 <avihay_cohen@campus.technion.ac.il>
2015-05-25 18:59:50 +0300

    commit - activity_item_info

---
+++ app/src/main/java/com/mycompany/veez/ItemInfoActivity.java
@@ -0,0 +1,14 @@
+package com.mycompany.veez;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ItemInfoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_info);
+    }
+}
