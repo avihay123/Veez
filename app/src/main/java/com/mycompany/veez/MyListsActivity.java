fd3875b69b11a716ca48f18281a35c0484459cb3
tomerbena <tomerbena@gmail.com>
2015-05-29 17:35:41 +0300

    commit - parse

--- app/src/main/java/com/mycompany/veez/MyListsActivity.java
+++ app/src/main/java/com/mycompany/veez/MyListsActivity.java
@@ -19,7 +19,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.content.res.Configuration;
 import java.util.ArrayList;
