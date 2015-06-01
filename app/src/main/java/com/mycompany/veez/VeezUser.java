6920c630fad15dbf4005b5a3502c194b049bc829
tomerbena <tomerbena@gmail.com>
2015-06-01 23:58:23 +0300

    recovered all facebook login after rollback. known bug: multiple token changes causes multiple activity starts.

--- app/src/main/java/com/mycompany/veez/VeezUser.java
+++ app/src/main/java/com/mycompany/veez/VeezUser.java
@@ -1,5 +1,7 @@
 package com.mycompany.veez;
 
+import android.graphics.Bitmap;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,29 +9,47 @@ import java.util.List;
  * Created by T on 5/27/2015.
  */
 public class VeezUser {
-    private String name; //TODO get from facebook;
-    private long id;
+    private String name;
+    private Bitmap profilePicture;
+    private String facebookID;
     private List<VeezList> myLists;
 
     public String getName() {
         return name;
     }
 
-    public VeezUser() {
-        name = null;
-        id = -1;
-        myLists = new ArrayList<VeezList>();
+    public VeezUser(String name, Bitmap profilePicutre, String facebookID){
+        this.name = name;
+        this.profilePicture = profilePicture;
+        this.facebookID = facebookID;
+        myLists = new ArrayList<>();
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public void setId(String id) {
+        this.facebookID = id;
     }
 
     public List<VeezList> getLists() {
         return myLists;
+    }
+
+    public String getFacebookID() {
+        return facebookID;
+    }
+
+    public void setFacebookID(String facebookID) {
+        this.facebookID = facebookID;
+    }
+
+    public Bitmap getBitmap() {
+        return profilePicture;
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        this.profilePicture = bitmap;
     }
 }
