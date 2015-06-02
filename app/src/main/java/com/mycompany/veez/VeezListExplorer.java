294c86dae803a7abc5de421cfe5698b4d53cb465
tomerbena <tomerbena@gmail.com>
2015-06-02 23:32:07 +0300

    finished - list filter. started moving tags view to buttons instead of headline

--- app/src/main/java/com/mycompany/veez/VeezListExplorer.java
+++ app/src/main/java/com/mycompany/veez/VeezListExplorer.java
@@ -12,11 +12,13 @@ public class VeezListExplorer {
     private List<VeezItem> items;
     private int likesCount;
     private Bitmap photo;
+    private List<String> tags;
 
-    public VeezListExplorer(int likesCount, List<VeezItem> items,String name) {
+    public VeezListExplorer(int likesCount, List<VeezItem> items,String name, List<String> tags) {
         this.name = name;
         this.items = items;
         this.likesCount = likesCount;
+        this.tags = tags;
     }
 
     public int getLikesCount() {
@@ -25,5 +27,18 @@ public class VeezListExplorer {
 
     public String getName() {
         return name;
+    }
+
+    public boolean hasTag(String tag){
+        return tags.contains(tag);
+    }
+
+    public boolean hasAllTags(List<String> searchTags){
+        for (String tag : searchTags) {
+            if (!tags.contains(tag)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
