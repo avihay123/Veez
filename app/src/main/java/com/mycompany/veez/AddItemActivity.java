2b09904d4395e21af67d2cb7e1dc3b4c871ce90c
Avihay123 <avihay_cohen@campus.technion.ac.il>
2015-06-03 03:36:59 +0300

    commit - list activity

--- app/src/main/java/com/mycompany/veez/AddItemActivity.java
+++ app/src/main/java/com/mycompany/veez/AddItemActivity.java
@@ -7,26 +7,10 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class AddItemActivity extends Activity {
-
-    private EditText et_item;
-    private EditText et_info;
-    private Button b_add_item_dialog;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
 
-        // TODO get the VeezList from dictionary
-
-        et_item = (EditText) findViewById(R.id.et_item);
-        et_info = (EditText) findViewById(R.id.et_info);
-
-        b_add_item_dialog = (Button) findViewById(R.id.b_add_item_dialog);
-        b_add_item_dialog.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-            }
-        });
     }
 }
